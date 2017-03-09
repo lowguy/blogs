f03ed09c27005e1fcd714a1e06340e2558266d81
System Administrator <root@haoyanpengdeiMac.local>
2017-03-09 19:31:59 +0800

    890

---
+++ js/index.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    author: '890',
+    message: '欢迎来到我的博客',
+    copyright:"blogs.890vip.cn",
+    items:[]
+  },
+  methods:{
+    getSubItem:function(index){
+      this.items[index]['subitem'] = [
+        {title:'sdsd'},
+        {title:'sdsd'},
+        {title:'sdsd'}
+      ]
+    }
+  },
+  mounted:function(){
+    this.$http.get('data/item.json').then((res) => {
+        console.log(res)
+        this.items = res.body.result
+    })
+  }
+})
