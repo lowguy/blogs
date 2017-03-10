f68316287e230e571dd1109af04ab6a42168d68f
System Administrator <root@haoyanpengdeiMac.local>
2017-03-10 09:51:31 +0800

    2017-03-10

---
+++ js/mysql.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    author: '890',
+    message: '欢迎来到我的博客'
+  },
+  methods:{
+    getSubItem:function(index,subindex){
+      var _this = this
+    },
+    getDetail:function(pid,cid){
+    }
+  },
+  mounted:function(){
+    var _this = this
+  }
+})
