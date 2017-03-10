503bd4c1ba52de09eb5dada5d6aeb413a0067c83
System Administrator <root@haoyanpengdeiMac.local>
2017-03-10 12:26:41 +0800

    2017-03-10

--- js/php.js
+++ js/php.js
@@ -4,6 +4,8 @@ var app = new Vue({
     author: '890',
     message: '欢迎来到我的博客',
     selected:1,
+    iscomment:flase,
+    placeholder_author:"请输入您的笔名",
     item:[],
     detail:[],
     comment:[]
@@ -28,6 +30,9 @@ var app = new Vue({
       },(res) => {
           console.log(res)
       })
+    },
+    comment:function(){
+      this.iscomment = !this.iscomment
     }
   },
   mounted:function(){
