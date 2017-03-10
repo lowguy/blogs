766552cc4b1a40ae09782cfbb5bb8d14e3ff16f0
System Administrator <root@haoyanpengdeiMac.local>
2017-03-10 13:30:36 +0800

    2017-03-10

--- js/php.js
+++ js/php.js
@@ -39,7 +39,7 @@ var app = new Vue({
         if(this.author && this.contents){
           this.comment_title = "评论"
           comments = {"author":this.author,"content":this.contents,"comment_time":"2017-03-10"}
-          this.comments.push(comments)
+          this.comment.push(comments)
           this.iscomment = !this.iscomment
         }
       }else{
