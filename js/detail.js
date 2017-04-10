6e8e20642eee6b61423748f259f4ef3a725be0e1
890 <810093817@qq.com>
2017-04-10 11:04:41 +0800

    site

--- js/detail.js
+++ js/detail.js
@@ -2,16 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     icp:"陕ICP备16003703号",
-    copyright:"Design by 890",
-    selected:1,
-    iscomment:false,
-    placeholder_author:"请输入您的笔名",
-    comment_title:"评论",
-    author_name:'',
-    contents:'',
-    item:[],
-    detail:[],
-    comment:[]
+    detail:[]
   },
   methods:{
     getDetail:function(id){
@@ -21,43 +12,6 @@ var app = new Vue({
       },(res) => {
           console.log(res)
       })
-      _this.getComment(id)
-    },
-    getComment:function(id){
-      var _this = this
-      this.$http.get('http://api.890vip.cn/api/blog/comment/id/'+id).then((res) => {
-          _this.comment = res.body.result
-      },(res) => {
-          console.log(res)
-      })
-    },
-    commentSub:function(){
-      if(this.iscomment){
-        if(this.author && this.contents){
-          this.comment_title = "评论"
-          var myDate = new Date();
-          var year = myDate.getFullYear()
-          var month = myDate.getMonth()
-          var day = myDate.getDate()
-          var comment_time = year +"-"+ month +"-"+ day
-          var storage=window.localStorage
-          var id = storage.getItem('id')
-          var _this = this
-          comments = {"author":this.author,"content":this.contents,"comment_time":comment_time}
-          this.$http.post('http://api.890vip.cn/api/blog/comments',{"id":id,"author":this.author,"content":this.contents},{emulateJSON:true}).then((res) => {
-              _this.comment.push(comments)
-              _this.iscomment = !_this.iscomment
-              _this.author = ''
-              _this.contents = ''
-          },(res) => {
-              console.log(res)
-          })
-         
-        }
-      }else{
-        this.comment_title = "提交"
-        this.iscomment = !this.iscomment
-      }
     }
   },
   mounted:function(){
