2c51794955c262f2cc6d9f6a5f2c5a536509bc10
890 <810093817@qq.com>
2017-04-14 14:40:01 +0800

    890

--- js/play.js
+++ js/play.js
@@ -13,7 +13,7 @@ var app = new Vue({
             var storage = window.localStorage
             var token = storage.getItem('token');
             console.log(token);
-            if(token == 0 && token == ''){
+            if(!token){
                 this.$http.post('http://api.890vip.cn/api/site/login',{"user_name":"890","user_pwd":"123456"},{emulateJSON:true}).then((res) => {
                     storage.setItem('token',res.body.token)
                 }, (res) => {
